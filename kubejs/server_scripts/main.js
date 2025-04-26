93bddfca9fc3371d6b68c7dbc3c1b469d9364d69
cyberpwn <dan@arcane.art>
2025-04-26 16:43:42 -0700

    Kubd

---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,25 @@
+LootJS.modifiers((event) => {
+    for(let i of ["tlc:chests/treasure",
+                "tlc:chests/dungeon",
+                "tlc:chests/house",
+                "tlc:chests/throne",
+                "tlc:chests/lab",
+                "minecraft:chests/stronghold/base", 
+                "underground_bunkers:chests/underground_bunker/underground_bunker_treasure", 
+                "underground_bunkers:chests/underground_bunker/underground_bunker_normal",
+                "underground_bunkers:chests/underground_bunker/underground_bunker_supply"]) {
+    event
+        .addLootTableModifier(i)
+        .randomChance(1)
+        .removeLoot("minecraft:gold_block")
+        .removeLoot("minecraft:gold_ingot")
+        .removeLoot("minecraft:diamond")
+        .removeLoot("minecraft:iron_ingot")
+        .removeLoot("minecraft:iron_block")
+        .randomChance(0.8)
+        .addLoot("minecraft:obsidian")
+        .randomChance(0.35)
+        .addLoot("minecraft:blaze_powder")
+        ;
+    }
+});

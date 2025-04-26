025c331f5cc516b6329905a85ed6e7d3985a46ac
cyberpwn <dan@arcane.art>
2025-04-26 16:41:12 -0700

    Update script to include kube

--- build_server.sh
+++ build_server.sh
@@ -8,6 +8,7 @@ mkdir -p server/{mods,config,scripts,essential}
 cp -a mods/. server/mods/
 cp -a config/. server/config/
 cp -a scripts/. server/scripts/
+cp -a kubejs/. server/kubejs/
 cp essential/config.toml server/essential/
 cp essential/onboarding.json server/essential/
 rm -rf server/mods/macos-input-fixes-1.8.jar
